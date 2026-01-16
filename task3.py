7f5e714a5ad2c77d6ba9295dbdc0568f3a0defe4
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-01-16 07:49:57 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,19 +5,20 @@ def main():
     choice1 = input("long body or round body? ")
     # CAUTION: You must include the word "body" when checking!
     if choice1 == "long body":
-         print("2) Roll the ball into an egg shape.\n")
+        print("2) Roll the ball into an egg shape.\n")
     else:
-         print("2) Keep it as a ball.\n")
+        print("2) Keep it as a ball.\n")
     print(f"3) Roll a smaller ball using {color1} for the head.\n")
-    print("4) Attach the head to one end of the body\n")
+    print("4) Attach the head to one end of the body.\n")
     choice2 = input("long tail or short tail? ")
     if choice2 == "long tail":
-         print(f"5) Roll a thin rope using {color2} and attach to the back.\n")
+        print(f"5) Roll a thin rope using {color2} and attach to the back.\n")
     else:
-         print(f"5) add a small bump {color2} to the back\n")
+        print(f"5) Add a small bump using {color2} to the back.\n")
     print(f"6) Add four small legs to the bottom using {color1}.\n")
     print("7) Add two dots for eyes and a tiny nose.\n")
-    print('8) Name this creation: "mouse"')
+    print('8) Name this creation: "Mouse"')
+
 
 if __name__ == "__main__":
     main()
